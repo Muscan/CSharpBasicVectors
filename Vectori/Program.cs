dbfe77ddce8dc976730bcb88d2482d2c45fe09c3
Mihai <kubikweg@gmail.com>
2021-07-19 16:25:43 +0300

    Feature: BinarySearch v.2

--- Vectori/Program.cs
+++ Vectori/Program.cs
@@ -8,15 +8,15 @@ namespace Vectori
     {
         static void Main(string[] args)
         {
-            int[] v = { 33, 32, 55, 756, 87878, 9 };
+            int[] v = { 33, 32, 55, 756, 87878, 9, 0, 1, -2 };
 
 
-            afisare(v);
+            //afisare(v);
             //v = stergere(v, 2);
-
-            stergere(v, 0);
+            //stergere(v, 0);
             //v = inserare(v,6 , 10);
-            afisare(v);
+            //afisare(v);
+            bubbleSort2(v);
 
 
 
